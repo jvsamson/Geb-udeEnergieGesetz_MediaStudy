16027a31a9c81e540ecd2b1ad2ee423f276d257e
jvsamson <222918@students.hertie-school.org>
2023-12-03 20:24:06 +0100

    sentinment german dict

---
+++ sentiment_SentiWS.py
@@ -0,0 +1,48 @@
+import pandas as pd
+
+# Function to load SentiWS into a dictionary
+def load_sentiws(file_path):
+    senti_dict = {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            elements = line.strip().split('\t')
+            if len(elements) < 2:
+                continue
+            try:
+                score = float(elements[-1])
+            except ValueError:
+                continue
+            words = elements[0].split(',')
+            for word in words:
+                senti_dict[word] = score
+    return senti_dict
+
+# Load SentiWS positive and negative lexicons
+sentiws_pos = load_sentiws('./SentiWS_v2/SentiWS_v2.0_Positive.txt')
+sentiws_neg = load_sentiws('./SentiWS_v2/SentiWS_v2.0_Negative.txt')
+
+# Load the processed DataFrame
+df_processed = pd.read_csv('df_processed.csv')
+
+# Function to calculate sentiment score
+def sentiment_score(text):
+    # Ensure that text is a string
+    if not isinstance(text, str):
+        return 0
+    tokens = text.split()
+    score = 0
+    for token in tokens:
+        if token in sentiws_pos:
+            score += sentiws_pos[token]
+        elif token in sentiws_neg:
+            score += sentiws_neg[token]
+    return score
+
+# Apply the sentiment analysis
+df_processed['Sentiment_Score'] = df_processed['Subtitle'].apply(sentiment_score)
+
+# Save or analyze the results
+df_processed.to_csv('df_sentiment_analysis.csv', index=False)
+
+# Optionally, print the first few rows to check
+print(df_processed.head())
