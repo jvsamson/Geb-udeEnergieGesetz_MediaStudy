62c1fd19261ff4c22252e4458524f0283dea3427
Fabian <78969880+FaMe-1337@users.noreply.github.com>
2023-12-03 10:40:55 +0100

    Processing
    Added df_processed and df_o-tones

--- subtitle_processing.py
+++ subtitle_processing.py
@@ -5,7 +5,22 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
 import string
 import spacy
+import os
 
+# Get the directory of the current script
+script_directory = os.path.dirname(__file__)
+
+# Change the current working directory to the script directory
+os.chdir(script_directory)
+
+# Now the current working directory is set to where the script is located
+# You can verify by printing the current working directory again
+print("Current working directory:", os.getcwd())
+
+# Load the dataframe from a file in the current working directory
+df_cleaned = pd.read_csv('cleaned_data_final.csv')
+
+print(df_cleaned)
 
 # Download stopwords from NLTK
 nltk.download('punkt')
@@ -17,13 +32,13 @@ nlp = spacy.load("de_core_news_sm")
 # Initialize the NLTK Porter Stemmer
 stemmer = PorterStemmer()
 
-# Function to preprocess text 
+# Function to preprocess text
 def preprocess_text(text):
     # Convert text to lowercase
     text = text.lower()
     
-    # Remove punctuation
-    text = text.translate(str.maketrans('', '', string.punctuation))
+    # Remove punctuation except for periods
+    text = text.translate(str.maketrans('', '', string.punctuation.replace('.', '')))
     
     # Tokenize text using spaCy for lemmatization
     doc = nlp(text)
@@ -33,11 +48,24 @@ def preprocess_text(text):
     stop_words = set(stopwords.words('german'))
     tokens = [word for word in tokens if word not in stop_words]
     
-    # Apply stemming using NLTK's Porter Stemmer
-    stemmed_tokens = [stemmer.stem(token) for token in tokens]
+    # Apply stemming using NLTK's Porter Stemmer (optional, since lemmatization is also used)
+    stemmed_tokens = [stemmer.stem(token) for token in tokens]  # You can comment this out if needed
     
-    return ' '.join(stemmed_tokens)
+    # Rejoin tokens into a string, keeping periods as they are
+    return ' '.join(stemmed_tokens)  # Use tokens instead of stemmed_tokens if stemming is not desired
 
 
-# load dataframe with all texts
-df_cleaned = pd.read_csv(file_path)
+# Apply the preprocessing to the 'subtitles' column and add a new column 'df_processed'
+df_cleaned['Processed'] = df_cleaned['Subtitle'].apply(preprocess_text)
+
+# Save the dataframe to a new CSV file if needed
+df_cleaned.to_csv('df_processed.csv', index=False)
+
+# Print the first few rows of the specified columns from df_cleaned
+print(df_cleaned[['Content Type', 'Speaker Name', 'Subtitle', 'Processed']].head())
+
+# Assuming df_cleaned has a 'Content Type' column after processing
+df_o_tone = df_cleaned.loc[df_cleaned['Content Type'] == 'O-Ton'].copy()
+df_o_tone = df_o_tone.drop(columns=['Segment Description'])
+df_o_tone.to_csv('df_o_tones.csv')
+# You can now work with df_o_tone which contains only the entries with Content Type as "O-Tone"
